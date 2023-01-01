9295855dba3b0850f706dac4c25f44039aae1909
louiiuol <louis.godlewski@vidmizer.com>
2023-01-01 16:38:36 +0100

    Generating ng user app

---
+++ apps/lmc-app-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'la-methode-claire-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+	title = 'lmc-app-ui';
+}
