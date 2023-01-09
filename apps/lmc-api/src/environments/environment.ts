fe586bf15b196dcaa61ae46bda547642014852d9
louiiuol <louis.godlewski@vidmizer.com>
2023-01-09 20:45:02 +0100

    🏗️ Sync NestJS environment files w/ .env file

--- apps/lmc-api/src/environments/environment.ts
+++ apps/lmc-api/src/environments/environment.ts
@@ -1,3 +1,5 @@
-export const environment = {
-	production: false,
-};
+//? Get `.env` local file to configure app
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const environment = process.env;
--- apps/lmc-api/src/main.ts
+++ apps/lmc-api/src/main.ts
@@ -4,19 +4,23 @@
  * This is only a minimal backend to get started.
  */
 
-import {Logger} from '@nestjs/common';
+import {Logger, ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 
 import {AppModule} from './app/app.module';
+import {GlobalExceptionFilter} from './app/core/exceptions/global-exceptions.filter';
+import {environment} from './environments/environment.prod';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	const globalPrefix = 'api';
+	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalFilters(new GlobalExceptionFilter());
+	const globalPrefix = environment.API_PREFIX;
 	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3333;
+	const port = environment.API_PORT || 3333;
 	await app.listen(port);
 	Logger.log(
-		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+		`🚀 Application is running on: http://${environment.API_HOST}:${port}/${globalPrefix}`
 	);
 }
 
