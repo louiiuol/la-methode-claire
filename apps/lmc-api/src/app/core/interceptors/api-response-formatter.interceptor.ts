7ba4a584dc9e09ed7361f73ecb92a850db01d954
louiiuol <louis.godlewski@vidmizer.com>
2023-01-30 00:20:00 +0100

    🧑‍💻 API Success responses DTO will now be wrapped in global interface

---
+++ apps/lmc-api/src/app/core/interceptors/api-response-formatter.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+	Injectable,
+	NestInterceptor,
+	ExecutionContext,
+	CallHandler,
+} from '@nestjs/common';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {APIResponse} from '../../models/api-response.model';
+
+/**
+ * Intercept all requests and return a response formatted as APIResponse DTO.
+ */
+@Injectable()
+export class APIResponseFormatter implements NestInterceptor {
+	intercept = (context: ExecutionContext, next: CallHandler): Observable<any> =>
+		next
+			.handle()
+			.pipe(map(res => new APIResponse(res, {ctx: context.switchToHttp()})));
+}
