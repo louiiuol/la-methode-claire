6222049fdc50ee0c6fb3502b95e6bd59cc3704b6
louiiuol <louis.godlewski@vidmizer.com>
2023-01-30 00:20:30 +0100

    ♻️ Refactoring APIResponse implementation

--- apps/lmc-api/src/app/models/api-response.model.ts
+++ apps/lmc-api/src/app/models/api-response.model.ts
@@ -6,24 +6,38 @@ import {
 } from '@lmc/api-interfaces';
 import {IncomingMessage} from 'http';
 
+type APIResponseDataType<T> = T | T[] | null;
+
 /**
  * Implementation of APIResponse for every response from this API. This will help us
  * to be more consistent with the rest of the codebase.
  */
-export class APIResponse<T extends AbstractViewDTO> implements IAPIResponse<T> {
+export class APIResponse<T> implements IAPIResponse<T> {
 	timestamp: string;
 	path: string;
 	messages: string[];
 	error: string;
-	data: T | T[] | null;
+	data: APIResponseDataType<T>;
 	paginator: PaginationConfig | undefined;
 
-	constructor(data?: T | T[], options?: APIResponseOptions) {
+	constructor(data: APIResponseDataType<T>, options: APIResponseOptions) {
+		const incomingMessage = options?.ctx?.getRequest<IncomingMessage>();
 		// TODO Improve format => expected: 29/01/2023, 22:57:41
 		this.timestamp = new Date().toISOString();
-		this.path = options?.ctx?.getRequest<IncomingMessage>()?.url;
-		this.setErrorMessages(options?.exception);
+		this.path = incomingMessage?.url;
 		this.data = data;
+		if (options?.exception) this.setErrorMessages(options?.exception);
+		else this.setSuccessMessages(incomingMessage);
+	}
+
+	private setSuccessMessages(input?: IncomingMessage): void {
+		const actions = {
+			POST: 'Creation',
+			PUT: 'Full update',
+			PATCH: 'Partial update',
+			DELETE: 'Deletion',
+		};
+		this.messages = [`${actions[input?.method]} has been achieved. 🎉`];
 	}
 
 	/**
@@ -36,6 +50,8 @@ export class APIResponse<T extends AbstractViewDTO> implements IAPIResponse<T> {
 		this.messages = validationResponse
 			? validationResponse.message
 			: [String(exception)];
-		this.error = validationResponse ? validationResponse.error : 'Query Failed';
+		this.error = validationResponse
+			? validationResponse.error
+			: 'Query Failed: unknown error occured 🎩';
 	};
 }
