0da97f5f6594f23de65e8c3dfcca623ed4c4502f
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-01 17:22:47 +0200

    Improved homepage

--- src/app/views/public-view/pages/home/home.page.ts
+++ src/app/views/public-view/pages/home/home.page.ts
@@ -1,11 +1,53 @@
-import {Component, HostBinding} from '@angular/core';
-import {CardComponent} from '@shared/components';
+import {NgFor} from '@angular/common';
+import {Component, HostBinding, forwardRef} from '@angular/core';
+import {TranslateModule} from '@core';
+import {ButtonComponent, CardComponent} from '@shared/components';
 
 @Component({
 	standalone: true,
-	imports: [CardComponent],
+	imports: [
+		NgFor,
+		forwardRef(() => TranslateModule),
+		CardComponent,
+		ButtonComponent,
+	],
 	templateUrl: 'home.page.html',
 })
 export class HomePage {
 	@HostBinding('class') protected class = '!block';
+
+	toto = 6;
+
+	methodExplanations = [
+		{
+			index: 1,
+			color: '#ac981a',
+			title: 'scripts',
+		},
+		{
+			index: 2,
+			color: '#fdeaa6',
+			title: 'lesson',
+		},
+		{
+			index: 3,
+			color: '#b2ced8',
+			title: 'exercice',
+		},
+		{
+			index: 4,
+			color: '#ef7e64',
+			title: 'dashboard',
+		},
+		{
+			index: 5,
+			color: '#fcdac6',
+			title: 'poster',
+		},
+		{
+			index: 6,
+			color: '#ef7f96',
+			title: 'newsletter',
+		},
+	];
 }
--- src/app/views/public-view/public.page.ts
+++ src/app/views/public-view/public.page.ts
@@ -1,47 +1,25 @@
-import {Component, HostBinding, forwardRef, inject} from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {Component, HostBinding, forwardRef} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {PlatformService} from '@core';
-import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './components';
 
 /**
- * Public View Container, defines global layout:
- * - Sidebar to present Application & display navigation on mobile
+ * Public View Container, defines global layout.
+ * - Sidebar to present Application & display navigation on mobile (not ready yet)
  * - Header with navigation links and Logo (responsive integrated)
  * - <router-outlet> to display children in maximized box (overflow handled)
  *
- * **This container must only contain public actions; At this point, user is authenticated yet !**
+ * **This container must only contain public actions; At this point, user isn't authenticated yet !**
  */
 @Component({
 	standalone: true,
-	imports: [
-		CommonModule,
-		MatSidenavModule,
-		forwardRef(() => RouterModule),
-		HeaderComponent,
-	],
+	imports: [forwardRef(() => RouterModule), HeaderComponent],
 	template: `
-		<mat-drawer-container class="h-full w-full" autosize>
-			<mat-drawer
-				#publicDrawer
-				[class]="platform.responsive('w-80', 'w-1/3', 'w-120')"
-				[mode]="platform.isMobileView() ? 'over' : 'side'">
-				hello
-			</mat-drawer>
-			<mat-drawer-content
-				style="display: flex !important;"
-				class="flex-1 flex-col">
-				<app-public-header
-					(toggledMobileMenu)="publicDrawer.toggle()"></app-public-header>
-				<main class="centered-content background-gradient">
-					<router-outlet></router-outlet>
-				</main>
-			</mat-drawer-content>
-		</mat-drawer-container>
+		<app-public-header />
+		<main>
+			<router-outlet></router-outlet>
+		</main>
 	`,
 })
 export class PublicPage {
 	@HostBinding('class') private readonly class = 'page';
-	protected readonly platform = inject(PlatformService);
 }
