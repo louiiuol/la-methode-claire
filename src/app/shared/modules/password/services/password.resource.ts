b202a867769bb47e1f7a5595fafc69361bd59d08
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-04 21:07:02 +0200

    🐛 Fixing routes

--- src/app/shared/modules/password/services/password.resource.ts
+++ src/app/shared/modules/password/services/password.resource.ts
@@ -13,16 +13,15 @@ export class PasswordResource extends HttpResource {
 
 	forgotPassword = (dto: {email: string}) =>
 		this.create(dto, {
-			path: 'forgotten-password',
-			notifyOnSuccess: true,
+			customResource: '',
+			path: 'forgot-password',
 			notifyOnError: false,
 			customAction: 'submit',
 		});
 
-	resetpassword = (token: string, dto: PasswordResetApiDto) =>
-		this.update(null, dto, {
-			path: 'change-password',
-			headers: new HttpHeaders({Authorization: 'Bearer ' + token}),
+	resetpassword = (uuid: string, dto: PasswordResetApiDto) =>
+		this.update(uuid, dto, {
+			path: 'reset-password',
 		});
 
 	updatePassword = (dto: PasswordUpdateDto) =>
