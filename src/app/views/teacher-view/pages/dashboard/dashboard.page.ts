636ff6ba13c4058156aac0ff3faf1aa744eb0821
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-04 21:15:54 +0200

    ♻️ Preparing components for next features

--- src/app/views/teacher-view/pages/dashboard/dashboard.page.ts
+++ src/app/views/teacher-view/pages/dashboard/dashboard.page.ts
@@ -1,11 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {AsyncPipe, NgFor, NgIf} from '@angular/common';
+import {Component, HostBinding, forwardRef} from '@angular/core';
+import {AuthService} from '@core';
+import {ButtonComponent} from '@shared/components';
+import {LibraryModule} from '@shared/modules/library/library.module';
+import {LibraryService} from '@shared/modules/library/services/library.service';
 
 @Component({
 	standalone: true,
+	imports: [
+		NgIf,
+		AsyncPipe,
+		NgFor,
+		forwardRef(() => LibraryModule),
+		ButtonComponent,
+	],
 	templateUrl: './dashboard.page.html',
 })
-export class DashboardPage implements OnInit {
-	constructor() {}
+export class DashboardPage {
+	@HostBinding('class') class = 'p-6';
 
-	ngOnInit() {}
+	lessons$ = this.library.getLibrary();
+
+	protected currentLesson = this.authenticator?.currentUser?.currentLesson ?? 0;
+
+	hasValidSubscription =
+		!!this.authenticator?.currentUser?.hasValidSubscription;
+
+	constructor(
+		private readonly library: LibraryService,
+		private readonly authenticator: AuthService
+	) {}
 }
