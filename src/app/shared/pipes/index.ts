200bcd2bb13c404888f9a6265a576f65e48885b9
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-04 21:08:52 +0200

    ✨ Init TrustUrlPipe

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -2,3 +2,4 @@
  * Handy angular pipes exported to ease formatting data in HTML templates
  */
 export {TrustHtmlPipe} from './trust-html.pipe';
+export {TrustUrlPipe} from './trust-url.pipe';
---
+++ src/app/shared/pipes/trust-url.pipe.ts
@@ -0,0 +1,18 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+
+/**
+ * Formats given url and makes it "safe" using DomSanitizer
+ *
+ * This pipe must be used with controlled content only (should be parsed and escaped)
+ */
+@Pipe({
+	standalone: true,
+	name: 'trustURL',
+})
+export class TrustUrlPipe implements PipeTransform {
+	constructor(private _sanitizer: DomSanitizer) {}
+	transform(value: string | null | undefined): SafeHtml {
+		return this._sanitizer.bypassSecurityTrustResourceUrl(value ?? '');
+	}
+}
