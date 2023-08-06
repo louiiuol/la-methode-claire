38b11efe07f69a526990cd15c6c7b787dd108a70
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-06 16:09:51 +0200

    🔧 simplify imports

--- src/app/shared/modules/library/library.module.ts
+++ src/app/shared/modules/library/library.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {LibraryResource} from './services/library.resource';
 import {LibraryService} from './services/library.service';
-import {CourseViewerComponent} from './components/course-viewer.component';
+import {CourseViewerComponent} from './components';
 
 @NgModule({
 	imports: [CourseViewerComponent],
