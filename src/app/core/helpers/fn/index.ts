c31d058b9a86bad87fab9be0d45b53f2663d5cf4
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-06 16:10:55 +0200

    ✨ Add isBoolean() helper function

--- src/app/core/helpers/fn/index.ts
+++ src/app/core/helpers/fn/index.ts
@@ -1,3 +1,4 @@
 export {untilDestroyed} from './until-destroyed.fn';
 export {capitalize} from './capitalize.fn';
 export {clean} from './clean.fn';
+export {isBoolean} from './is-boolean.fn';
---
+++ src/app/core/helpers/fn/is-boolean.fn.ts
@@ -0,0 +1,1 @@
+export const isBoolean = (prop: unknown) => typeof prop === 'boolean';
