d2371057b3a1864a62a4cddedbfae04ec848f46e
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-10 11:51:21 +0200

    ⚡️  Caching lessons when already fetched

--- src/app/shared/modules/library/services/library.service.ts
+++ src/app/shared/modules/library/services/library.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {LibraryResource} from './library.resource';
-import {map} from 'rxjs';
+import {map, of} from 'rxjs';
+import {CourseViewDto} from '../types/course-view.dto';
 
 /**
  * Provides methods to handle user's library
@@ -9,9 +10,19 @@ import {map} from 'rxjs';
  */
 @Injectable()
 export class LibraryService {
+	private lessons?: CourseViewDto[] | null;
 	constructor(private readonly http: LibraryResource) {}
 
-	getLibrary = () => this.http.getLibrary().pipe(map(lessons => lessons.value));
+	getLibrary = () =>
+		this.lessons
+			? of(this.lessons)
+			: this.http.getLibrary().pipe(
+					map(lessons => {
+						const fetchedLessons = lessons.value;
+						this.lessons = fetchedLessons;
+						return fetchedLessons;
+					})
+			  );
 
 	setCurrentLesson = (index: number) => this.http.setCurrentLessonIndex(index);
 }
