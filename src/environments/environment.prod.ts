38a0260b4a05e31a952e44dde4af74d7a570eee5
Louis Godlewski <louis.godlewski@gmail.com>
2023-08-10 18:01:02 +0200

    🔧 Update api URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
 	hmr: false,
-	root_url: 'https://glorious-kit-refined.ngrok-free.app/api',
+	root_url: 'https://lmc-api-production.up.railway.app',
 };
