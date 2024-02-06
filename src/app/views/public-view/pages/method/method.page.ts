d77ddfaab38bafc09774d03fbe7308ee65973d1f
Louis Godlewski <louis.godlewski@gmail.com>
2024-02-06 13:00:28 +0100

    ✨ La méthode: progression

--- src/app/views/public-view/pages/method/method.page.ts
+++ src/app/views/public-view/pages/method/method.page.ts
@@ -1,9 +1,18 @@
-import {Component, HostBinding} from '@angular/core';
+import {AsyncPipe, NgFor, NgIf} from '@angular/common';
+import {Component, HostBinding, inject} from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {LibraryModule} from '@shared/modules/library/library.module';
+import {LibraryService} from '@shared/modules/library/services/library.service';
 
 @Component({
 	standalone: true,
+	imports: [NgIf, NgFor, AsyncPipe, MatTableModule, LibraryModule],
 	templateUrl: 'method.page.html',
 })
 export class MethodPage {
 	@HostBinding('class') class = 'block pb-16';
+
+	protected readonly lessons$ = inject(LibraryService).getLibrary();
+
+	displayedColumns: string[] = ['index', 'name'];
 }
